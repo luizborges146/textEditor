09d5b1636710bd5f18019072443762a21fe71af8
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-11-30 21:58:07 -0800

    Fixed the issue related to the Manifest and service workes, the issue now is with the session storage not being saved

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -33,16 +33,16 @@ export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-    const contactDb = await openDB('jate', 1);
+    const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.get(1);
+  const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request;
